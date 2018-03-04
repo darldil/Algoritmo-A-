5d7199c07ac713db3735e88019e98292649ba3c3
darldil <mabbati@ucm.es>
2018-03-04 12:30:47 +0100

    1.0

--- Practica1_A/src/main/Main.java
+++ Practica1_A/src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import ui.Controlador;
 import ui.Window;
 
@@ -8,6 +11,24 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Controlador controlador = new Controlador();
+		
+		 try {
+	            // Set cross-platform Java L&F (also called "Metal")
+	        UIManager.setLookAndFeel(
+	            UIManager.getCrossPlatformLookAndFeelClassName());
+	    } 
+	    catch (UnsupportedLookAndFeelException e) {
+	       // handle exception
+	    }
+	    catch (ClassNotFoundException e) {
+	       // handle exception
+	    }
+	    catch (InstantiationException e) {
+	       // handle exception
+	    }
+	    catch (IllegalAccessException e) {
+	       // handle exception
+	    }
 		
 		new Window(controlador);
 		
